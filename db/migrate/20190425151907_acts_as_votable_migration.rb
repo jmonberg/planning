95e4df2e573634a0e38b7ea5b134fa4e18f265b2
zbohenick <zbohenick@gmail.com>
2019-10-29 12:38:25 -0400

    add vote count preview to improvements

--- db/migrate/20190425151907_acts_as_votable_migration.rb
+++ db/migrate/20190425151907_acts_as_votable_migration.rb
@@ -1,22 +1,22 @@
-class ActsAsVotableMigration < ActiveRecord::Migration
+class AddCachedVotesToPosts < ActiveRecord::Migration
   def self.up
-    create_table :votes do |t|
+    add_column :improvements, :cached_votes_total, :integer, :default => 0
+    add_column :improvements, :cached_votes_score, :integer, :default => 0
+    add_column :improvements, :cached_votes_up, :integer, :default => 0
+    add_column :improvements, :cached_votes_down, :integer, :default => 0
+    add_index  :improvements, :cached_votes_total
+    add_index  :improvements, :cached_votes_score
+    add_index  :improvements, :cached_votes_up
+    add_index  :improvements, :cached_votes_down
 
-      t.references :votable, :polymorphic => true
-      t.references :voter, :polymorphic => true
-
-      t.boolean :vote_flag
-      t.string :vote_scope
-      t.integer :vote_weight
-
-      t.timestamps
-    end
-
-    add_index :votes, [:voter_id, :voter_type, :vote_scope]
-    add_index :votes, [:votable_id, :votable_type, :vote_scope]
+    Uncomment this line to force caching of existing votes
+    Improvements.find_each(&:update_cached_votes)
   end
 
   def self.down
-    drop_table :votes
+    remove_column :improvements, :cached_votes_total
+    remove_column :improvements, :cached_votes_score
+    remove_column :improvements, :cached_votes_up
+    remove_column :improvements, :cached_votes_down
   end
 end
